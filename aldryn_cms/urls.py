95a2e3dffb03b34b26dd0a6e170b2e190779335b
Paulo <commonzenpython@gmail.com>
2015-09-23 21:29:14 -0400

    adds views/urls for check uninstall

--- aldryn_cms/urls.py
+++ aldryn_cms/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import url, include
 
 from cms.sitemaps import CMSSitemap
 
+from .views import check_uninstall_ok
+
 
 urlpatterns = [
+    # Not sure if we should hardcode admin path like this
+    url(r'^admin/~cmscloud-api/check-uninstall/$', check_uninstall_ok),
     url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^robots\.txt$', include('robots.urls')),
 ]
