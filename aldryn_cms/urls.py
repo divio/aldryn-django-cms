72311c9c8e062997ba03fd6f1e6f014f2155e9b7
Paulo <commonzenpython@gmail.com>
2015-09-27 00:11:47 -0400

    adds sso integration

--- aldryn_cms/urls.py
+++ aldryn_cms/urls.py
@@ -7,8 +7,7 @@ from .views import check_uninstall_ok
 
 
 urlpatterns = [
-    # Not sure if we should hardcode admin path like this
-    url(r'^admin/~cmscloud-api/check-uninstall/$', check_uninstall_ok),
+    url(r'^admin/~cmscloud-api/check-uninstall/$', check_uninstall_ok, name='cms-check-uninstall'),
     url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^robots\.txt$', include('robots.urls')),
 ]
