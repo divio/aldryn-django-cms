4b65da02bb6d9d08a4e1f9f54a6ff02af1f58f43
Paulo <commonzenpython@gmail.com>
2015-10-01 09:14:20 -0400

    pin hvad to < 1.0.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         'django-robots',
         'aldryn-boilerplates',
         'django-filer',
-        'django-hvad',
+        'django-hvad<1.0.0',
         'aldryn-snake',
 
         # default plugins
