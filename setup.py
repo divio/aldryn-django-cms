98f97b7dd948aca5c0e0e4abe4c528c5c6f6b685
Martin Koistinen <mkoistinen@gmail.com>
2016-01-27 14:19:51 +0100

    Force latest version of boilerplates

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         # Other common
         # ------------
         # TODO: mostly to be split out into other packages
-        'aldryn-boilerplates',
+        'aldryn-boilerplates>=0.7.4',
         'aldryn-snake',
         'BeautifulSoup',
         'django-compressor',
