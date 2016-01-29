eed51715c6e8abfe909575020be575f4a9f16007
Martin Koistinen <mkoistinen@gmail.com>
2016-01-29 13:40:23 +0100

    Bump to official release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'django-cms==3.2.1.2',
+        'django-cms==3.2.1',
         'requests',
 
         # Default plugins
