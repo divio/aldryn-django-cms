e7b6fedef2a6cb02b42d399a2781ec1249e219cc
Stefan Foulis <stefan@foulis.ch>
2016-03-03 09:46:54 +0100

    remove line end limit regex ($)
    Django 1.9 complains about this when used with an include.

--- aldryn_django_cms/urls.py
+++ aldryn_django_cms/urls.py
@@ -10,5 +10,5 @@ from .views import check_uninstall_ok
 urlpatterns = [
     url(r'^admin/~cmscloud-api/check-uninstall/$', check_uninstall_ok, name='cms-check-uninstall'),
     url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {'cmspages': CMSSitemap}}),
-    url(r'^robots\.txt$', include('robots.urls')),
+    url(r'^robots\.txt', include('robots.urls')),
 ]
