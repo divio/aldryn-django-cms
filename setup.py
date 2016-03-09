df73c18057fe4360b761f263c928c2ab6ec18162
Martin Koistinen <mkoistinen@gmail.com>
2016-03-09 17:31:07 -0500

    Bump to 3.2.3.0 for django CMS 3.2.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'django-cms==3.2.2',
+        'django-cms==3.2.3',
         'requests',
 
         # Default plugins
