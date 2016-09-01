70b11f815bc8987e2e10beee099139a4a170e5b3
Paulo <commonzenpython@gmail.com>
2016-09-01 18:47:23 -0400

    Removed addons which now support Aldryn on their own (#67)

--- aldryn_config.py
+++ aldryn_config.py
@@ -49,7 +49,6 @@ class Form(forms.BaseForm):
         from functools import partial
         from django.core.urlresolvers import reverse_lazy
         from aldryn_addons.utils import boolean_ish, djsenv
-        from aldryn_django import storage
 
         env = partial(djsenv, settings=settings)
 
@@ -233,103 +232,15 @@ class Form(forms.BaseForm):
         # django-robots
         settings['INSTALLED_APPS'].append('robots')
 
-        # django-filer
-        settings['INSTALLED_APPS'].extend([
-            'filer',
-            'easy_thumbnails',
-            'mptt',
-            'polymorphic',
-        ])
-        settings['FILER_DEBUG'] = boolean_ish(env('FILER_DEBUG', settings['DEBUG']))
-        settings['FILER_ENABLE_LOGGING'] = boolean_ish(env('FILER_ENABLE_LOGGING', True))
-        settings['FILER_IMAGE_USE_ICON'] = True
-        settings['ADDON_URLS'].append(
-            'filer.server.urls'
-        )
-        settings.setdefault('MEDIA_HEADERS', []).insert(0, (
-            r'filer_public(?:_thumbnails)?/.*',
-            {
-                'Cache-Control': 'public, max-age={}'.format(86400 * 365),
-            },
-        ))
-
-        # easy-thumbnails
-        settings['INSTALLED_APPS'].extend([
-            'easy_thumbnails',
-        ])
-        settings['THUMBNAIL_QUALITY'] = env('THUMBNAIL_QUALITY', 90)
-        # FIXME: enabling THUMBNAIL_HIGH_RESOLUTION causes timeouts/500!
-        settings['THUMBNAIL_HIGH_RESOLUTION'] = False
-        settings['THUMBNAIL_PRESERVE_EXTENSIONS'] = ['png', 'gif']
-        settings['THUMBNAIL_PROCESSORS'] = (
-            'easy_thumbnails.processors.colorspace',
-            'easy_thumbnails.processors.autocrop',
-            'filer.thumbnail_processors.scale_and_crop_with_subject_location',
-            'easy_thumbnails.processors.filters',
-        )
-        settings['THUMBNAIL_SOURCE_GENERATORS'] = (
-            'easy_thumbnails.source_generators.pil_image',
-        )
-        settings['THUMBNAIL_CACHE_DIMENSIONS'] = True
-
-        # easy_thumbnails uses django's default storage backend (local file
-        # system storage) by default, even if the DEFAULT_FILE_STORAGE setting
-        # points to something else.
-        # If the DEFAULT_FILE_STORAGE has been set to a value known by
-        # aldryn-django, then use that as THUMBNAIL_DEFAULT_STORAGE as well.
-        for storage_backend in storage.SCHEMES.values():
-            if storage_backend == settings['DEFAULT_FILE_STORAGE']:
-                settings['THUMBNAIL_DEFAULT_STORAGE'] = storage_backend
-                break
-
         settings['MIGRATION_COMMANDS'].append(
             'python manage.py cms fix-tree'
         )
 
         # default plugins
         settings['INSTALLED_APPS'].extend([
-            'djangocms_text_ckeditor',
-            'djangocms_link',
-            'djangocms_snippet',
-            'djangocms_googlemap',
-            # cmsplugin-filer
-            'cmsplugin_filer_file',
-            'cmsplugin_filer_image',
-
             # required by aldryn-forms
             'captcha',
         ])
-
-        # boilerplate must provide /static/js/modules/ckeditor.wysiwyg.js and /static/css/base.css
-        CKEDITOR_SETTINGS = {
-            'height': 300,
-            'language': '{{ language }}',
-            'toolbar': 'CMS',
-            'skin': 'moono',
-            'extraPlugins': 'cmsplugins',
-            'toolbar_HTMLField': [
-                ['Undo', 'Redo'],
-                ['cmsplugins', '-', 'ShowBlocks'],
-                ['Format', 'Styles'],
-                ['TextColor', 'BGColor', '-', 'PasteText', 'PasteFromWord'],
-                ['Maximize', ''],
-                '/',
-                ['Bold', 'Italic', 'Underline', '-', 'Subscript', 'Superscript', '-', 'RemoveFormat'],
-                ['JustifyLeft', 'JustifyCenter', 'JustifyRight'],
-                ['HorizontalRule'],
-                ['Link', 'Unlink'],
-                ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Table'],
-                ['Source'],
-                ['Link', 'Unlink', 'Anchor'],
-            ],
-        }
-        boilerplate_name = settings['ALDRYN_BOILERPLATE_NAME']
-        if boilerplate_name == 'bootstrap3':
-            CKEDITOR_SETTINGS['stylesSet'] = 'default:/static/js/addons/ckeditor.wysiwyg.js'
-            CKEDITOR_SETTINGS['contentsCss'] = ['/static/css/base.css']
-        else:
-            CKEDITOR_SETTINGS['stylesSet'] = 'default:/static/js/modules/ckeditor.wysiwyg.js'
-            CKEDITOR_SETTINGS['contentsCss'] = ['/static/css/base.css']
 
         # select2 (required by djangocms_link plugin)
         settings['INSTALLED_APPS'].extend([
