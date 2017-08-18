f9c1bdd4043320e821dd79fe408897bf00117eaf
Paulo <commonzenpython@gmail.com>
2017-08-18 09:36:01 -0400

    Release 3.4.5.1.dev21 - django-cms 3.4.5.dev21

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'django-cms==3.4.4',
+        'django-cms==3.4.5.dev21',
         'requests',
 
         # NOTE: django-cms doesn't require this, but many of the addons do.
