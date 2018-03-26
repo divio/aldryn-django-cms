1bbe82ab0b4865acb1e7c1658253b27bf8c01a93
Paulo Alvarado <commonzenpython@gmail.com>
2018-03-26 17:41:01 -0400

    Bumped version to 3.4.6.2 - djangoCMS 3.4.6 (#100)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'django-cms==3.4.6rc1',
+        'django-cms==3.4.6',
         'requests',
 
         # NOTE: django-cms doesn't require this, but many of the addons do.
