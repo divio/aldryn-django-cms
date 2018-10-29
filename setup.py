19da00f9b462667cc7608db6812c4503b0adbb3e
Krzysztof Socha <ksocha@ksocha.com>
2018-10-29 08:43:46 +0000

    Release 4.0.0.7 - django-cms 4.0.0dev6 (#107)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'django-cms==4.0.0dev5',
+        'django-cms==4.0.0dev6',
         'requests',
 
         # NOTE: django-cms doesn't require this, but many of the addons do.
