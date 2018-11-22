e7eef4437c0e11b8084d98199d85b6bf40bffbca
Angelo Dini <angelo.dini@divio.ch>
2018-11-22 17:07:47 +0100

    update urlresolvers

--- aldryn_config.py
+++ aldryn_config.py
@@ -55,7 +55,7 @@ class Form(forms.BaseForm):
 
     def to_settings(self, data, settings):
         from functools import partial
-        from django.core.urlresolvers import reverse_lazy
+        from django.urls import reverse_lazy
         from aldryn_addons.utils import boolean_ish, djsenv
 
         env = partial(djsenv, settings=settings)
