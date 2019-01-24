772a5241e317381e2b6ce4aa8b942c7049eb3ce5
Bartosz Płóciennik <vThaian@users.noreply.github.com>
2019-01-24 11:29:05 +0100

    Release 3.6.0.6 - django-cms 3.6.0rc3 (#120)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'django-cms==3.6.0rc1',
+        'django-cms==3.6.0rc3',
         'requests',
 
         # NOTE: django-cms doesn't require this, but many of the addons do.
