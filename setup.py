eb7db249ae96ff30841f1ddbe2cec5424a70e0ae
Adam Murray <adam-murray@hotmail.co.uk>
2021-07-28 21:39:47 +0100

    Bumped version to 4.0.0.15 (#160)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'django-cms==4.0.0dev10',
+        'django-cms==4.0.0dev11',
         'requests',
 
         # NOTE: django-cms doesn't require this, but many of the addons do.
