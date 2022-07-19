d69aa39a052ccb3f6cd59075bd1153c9fd769ad2
Dennis Schwertel <dennisschwertel@gmail.com>
2022-07-19 15:18:17 +0200

    Release 4.0.0.17.dev13

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'django-cms==4.0.0dev12',
+        'django-cms==4.0.0dev13',
         'requests',
 
         # NOTE: django-cms doesn't require this, but many of the addons do.
