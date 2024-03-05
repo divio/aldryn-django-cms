651cbf05fd648c934ed7c595c09ad574a578f629
Angelo Dini <angelo.dini@divio.ch>
2024-03-05 10:43:13 +0100

    Release version 4.0.1.15 (#166)
    * update code base to other aldryn-django-cms versions
    
    * use my version for now
    
    * disable for now
    
    * bump version

--- aldryn_django_cms/urls.py
+++ aldryn_django_cms/urls.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import url, include
 from django.contrib.sitemaps.views import sitemap
+from django.urls import include, re_path
 
 from cms.sitemaps import CMSSitemap
 
@@ -9,7 +7,7 @@ from .views import check_uninstall_ok
 
 
 urlpatterns = [
-    url(r'^admin/~cmscloud-api/check-uninstall/$', check_uninstall_ok, name='cms-check-uninstall'),
-    url(r'^sitemap.xml$', sitemap, {'sitemaps': {'cmspages': CMSSitemap}}),
-    url(r'^robots\.txt', include('robots.urls')),
+    re_path(r'^admin/~cmscloud-api/check-uninstall/$', check_uninstall_ok, name='cms-check-uninstall'),
+    re_path(r'^sitemap.xml$', sitemap, {'sitemaps': {'cmspages': CMSSitemap}}),
+    re_path(r'^robots\.txt', include('robots.urls')),
 ]
--- setup.py
+++ setup.py
@@ -1,41 +1,71 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+from setuptools import find_packages, setup
 
-from setuptools import setup, find_packages
 from aldryn_django_cms import __version__
 
+
+REQUIREMENTS = [
+    # 'django-cms',
+
+    'aldryn-addons',
+    'requests',
+
+    # NOTE: django-cms doesn't require this, but many of the addons do.
+    #       If it is used, however, then it must be >=1.0.9 for CMS 3.3+.
+    'django-treebeard>=4.0.1',         # django-cms
+    'djangocms-admin-style',           # django-cms
+    'django-select2>=6.2',
+
+    # Other common
+    # ------------
+    # TODO: mostly to be split out into other packages
+    'aldryn-snake',
+    'django-compressor',
+    'django-parler',
+    # Django Sortedm2m 1.3 introduced a regression, that was fixed in 1.3.2
+    # See https://github.com/gregmuellegger/django-sortedm2m/issues/80
+    'django-sortedm2m>=1.5.0',
+    'django-robots',
+    'django-simple-captcha<=0.5.20',
+    'lxml',
+    'YURL',
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Framework :: Django',
+    'Framework :: Django :: 3.2',
+    'Framework :: Django :: 4.2',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
+
 setup(
-    name="aldryn-django-cms",
+    name='aldryn-django-cms',
     version=__version__,
-    description='An opinionated django CMS setup bundled as an Aldryn Addon',
     author='Divio AG',
     author_email='info@divio.ch',
-    url='https://github.com/aldryn/aldryn-django-cms',
+    url='https://github.com/divio/aldryn-django-cms',
+    license='BSD-3-Clause',
+    description='An opinionated django CMS setup bundled as an Divio Cloud addon',
+    long_description=open('README.rst').read(),
     packages=find_packages(),
-    install_requires=(
-        'aldryn-addons',
-        'django-cms==4.0.1.dev2',
-        'requests',
-
-        # NOTE: django-cms doesn't require this, but many of the addons do.
-        'django-treebeard>=4.0.1',         # django-cms
-        'djangocms-admin-style',           # django-cms
-        'django-select2>=4.3',
-
-        # Other common
-        # ------------
-        # TODO: mostly to be split out into other packages
-        'aldryn-snake',
-        'django-compressor',
-        'django-parler',
-        # Django Sortedm2m 1.3 introduced a regression, that was fixed in 1.3.2
-        # See https://github.com/gregmuellegger/django-sortedm2m/issues/80
-        'django-sortedm2m>=1.2.2,!=1.3.0,!=1.3.1',
-        'django-robots',
-        'django-simple-captcha',
-        'lxml',
-        'YURL',
-        'psycopg2',
-    ),
     include_package_data=True,
     zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
 )
