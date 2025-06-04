8af7e205fce0ed68904da4302b4d707ed6f073a3
Angelo Dini <angelo.dini@divio.ch>
2025-06-04 09:42:25 +0200

    Bumped version to 4.1.6.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from aldryn_django_cms import __version__
 
 
 REQUIREMENTS = [
-    'django-cms==4.1.5',
+    'django-cms==4.1.6',
 
     'aldryn-addons',
     'requests',
@@ -55,7 +55,7 @@ CLASSIFIERS = [
 
 
 setup(
-    name='aldryn_django_cms',
+    name='aldryn-django-cms',
     version=__version__,
     author='Divio AG',
     author_email='info@divio.ch',
